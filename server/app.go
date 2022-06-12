baa7339d06f7960f70ddfc5a72d10ff7bfaaa841
Nick Pizzigati <npizzigati@gmail.com>
2022-06-12 10:08:55 -0500

    Remove stale commented-out code

--- server/app.go
+++ server/app.go
@@ -1186,10 +1186,6 @@ func runFile(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	case "sql":
 		cn.runner.Write([]byte("\\i code.sql\n"))
 		room.setEventListener("newline", func(config eventConfig) {
-			// lines, err := strconv.Atoi(linesOfCode)
-			// if err != nil {
-			// 	fmt.Println("strconv error: ", err)
-			// }
 			if config.count == 1 {
 				room.echo = true
 				room.removeEventListener("newline")
