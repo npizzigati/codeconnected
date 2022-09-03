c93151e7af9f82226f0fd24e9825896efca0cc97
Nick Pizzigati <npizzigati@gmail.com>
2022-09-02 21:55:45 -0500

    Fix Invite issues
    - Document event listeners now removed on unmount
    - Dialog fades in and fades out (fades out automatically on copy
      link to clipboard

--- frontend/src/pages/components/Invite.jsx
+++ frontend/src/pages/components/Invite.jsx
@@ -4,27 +4,19 @@ import React, { useState, useEffect, useRef } from 'react';
 import { handlePointerDown } from '../../helpers/miscUtils.js';
 
 function Invite () {
-  const [showDialog, setShowDialog] = useState(false);
   const [popupMessage, setPopupMessage] = useState('');
   const inviteDialogDomRef = useRef(null);
   const inviteButtonDomRef = useRef(null);
 
   useEffect(() => {
-    function handleDocPointerDown (ev) {
-      if (!inviteDialogDomRef.current) {
-        return;
-      }
-      if (!inviteDialogDomRef.current.contains(ev.target) && !inviteButtonDomRef.current.contains(ev.target)) {
-        console.log(ev.target);
-        console.log('hiding dialog');
-        setShowDialog(false);
-      }
-    }
-
-    document.addEventListener('pointerdown', handleDocPointerDown);
-
+    document.addEventListener('pointerdown', inviteHandleDocPointerDown);
     // If escape key custom event fires, close this component's modal dialog
-    document.addEventListener('escapePressed', () => setShowDialog(false));
+    document.addEventListener('escapePressed', () => hideInviteDialog());
+
+    return function cleanup () {
+      document.removeEventListener('escapePressed', hideInviteDialog);
+      document.removeEventListener('pointerdown', inviteHandleDocPointerDown);
+    }
   });
 
   return (
@@ -35,48 +27,59 @@ function Invite () {
       <div
         className='invite__button'
         ref={inviteButtonDomRef}
-        onPointerDown={ev => handlePointerDown(ev, toggleInviteDialog)}
+        onPointerDown={ev => handlePointerDown(ev, showInviteDialog)}
       >
         <span>Invite</span>
       </div>
-      {showDialog &&
-        <div className='invite__dialog' ref={inviteDialogDomRef}>
-          <div className='u-marg-bot-1'>
-            <span className='invite__instructions'>
-              Send this link to a friend to join this code session:
-            </span>
+      <div className='invite__dialog hidden' ref={inviteDialogDomRef}>
+        <div className='u-marg-bot-1'>
+          <span className='invite__instructions'>
+            Send this link to a friend to join this code session:
+          </span>
+        </div>
+        <div className='media'>
+          <div className='media__text'>
+            <span className='invite__text'>{window.location.href}</span>
           </div>
-          <div className='media'>
-            <div className='media__text'>
-              <span className='invite__text'>{window.location.href}</span>
-            </div>
-            <div className='media__image-container'>
-              <img
-                className='media__image--micro media__image--clickable'
-                src='./images/copy.png'
-                onPointerDown={ev => handlePointerDown(ev, copyLinkToClipboard)}
-              />
-            </div>
+          <div className='media__image-container'>
+            <img
+              className='media__image--micro media__image--clickable'
+              src='./images/copy.png'
+              onPointerDown={ev => handlePointerDown(ev, copyLinkToClipboard)}
+            />
           </div>
         </div>
-      }
+      </div>
     </>
   );
 
-  function toggleInviteDialog () {
-    setShowDialog(!showDialog);
+  function inviteHandleDocPointerDown (ev) {
+    if (!inviteDialogDomRef.current) {
+      return;
+    }
+    if (!inviteDialogDomRef.current.contains(ev.target) && !inviteButtonDomRef.current.contains(ev.target)) {
+      hideInviteDialog();
+    }
+  }
+
+  function showInviteDialog () {
+    inviteDialogDomRef.current.classList.remove('hidden');
+  }
+
+  function hideInviteDialog () {
+    inviteDialogDomRef.current.classList.add('hidden');
   }
 
   function copyLinkToClipboard () {
     navigator.clipboard.writeText(window.location.href);
     showPopup('Invite link copied to your clipboard');
+    hideInviteDialog();
   }
 
   function showPopup (message) {
     setPopupMessage(message);
     setTimeout(() => {
       setPopupMessage('');
-      setShowDialog(false);
     }, 2000);
   }
 }
